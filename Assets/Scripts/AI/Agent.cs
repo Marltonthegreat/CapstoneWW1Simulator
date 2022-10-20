85c97059ad72c0b4a4fb9b2702b2cbd2a3718e25
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-10-20 16:33:53 -0600

    Imported GAT420

--- Assets/Scripts/AI/Agent.cs
+++ Assets/Scripts/AI/Agent.cs
@@ -27,7 +27,60 @@ public class Agent : MonoBehaviour
     private void Start()
     {
         stateMachine.AddState(new IdleState(this, typeof(IdleState).Name));
-        stateMachine.AddState(new PatrolState(this, typeof(PatrolState).Name));
+        //stateMachine.AddState(new PatrolState(this, typeof(PatrolState).Name));
+        stateMachine.AddState(new ChaseState(this, typeof(ChaseState).Name));
+        stateMachine.AddState(new DeathState(this, typeof(DeathState).Name));
+        stateMachine.AddState(new AttackState(this, typeof(AttackState).Name));
+        stateMachine.AddState(new RetreatState(this, typeof(RetreatState).Name));
+
+        //IdleState transitions
+        stateMachine.AddTransition(typeof(IdleState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 30) }), typeof(RetreatState).Name);
+        stateMachine.AddTransition(typeof(IdleState).Name, new Transition(new Condition[] { new BoolCondition(enemySeen, true), new FloatCondition(health, Condition.Predicate.GREATER, 30) }), typeof(ChaseState).Name);
+        stateMachine.AddTransition(typeof(IdleState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(DeathState).Name);
+
+        //stateMachine.AddTransition(typeof(PatrolState).Name, new Transition(new Condition[] { new BoolCondition(enemySeen, true), new FloatCondition(health, Condition.Predicate.GREATER, 30) }), typeof(ChaseState).Name);
+        //stateMachine.AddTransition(typeof(PatrolState).Name, new Transition(new Condition[] { new BoolCondition(enemySeen, true), new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 30) }), typeof(RetreatState).Name);
+        //stateMachine.AddTransition(typeof(PatrolState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(DeathState).Name);
+
+        stateMachine.AddTransition(typeof(ChaseState).Name, new Transition(new Condition[] { new BoolCondition(enemySeen, false) }), typeof(IdleState).Name);
+        stateMachine.AddTransition(typeof(ChaseState).Name, new Transition(new Condition[] { new FloatCondition(enemyDistance, Condition.Predicate.LESS_EQUAL, attackDistance), new FloatCondition(enemyHealth, Condition.Predicate.GREATER, 0) }), typeof(AttackState).Name);
+        stateMachine.AddTransition(typeof(ChaseState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 30) }), typeof(RetreatState).Name);
+        stateMachine.AddTransition(typeof(ChaseState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(DeathState).Name);
+
+        stateMachine.AddTransition(typeof(AttackState).Name, new Transition(new Condition[] { new FloatCondition(timer, Condition.Predicate.LESS_EQUAL, 0) }), typeof(ChaseState).Name);
+        stateMachine.AddTransition(typeof(AttackState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 30) }), typeof(RetreatState).Name);
+        stateMachine.AddTransition(typeof(AttackState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(DeathState).Name);
+
+        stateMachine.AddTransition(typeof(RetreatState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(IdleState).Name);
+        stateMachine.AddTransition(typeof(RetreatState).Name, new Transition(new Condition[] { new FloatCondition(health, Condition.Predicate.LESS_EQUAL, 0) }), typeof(DeathState).Name);
+
+        stateMachine.SetState(stateMachine.StateFromName(typeof(IdleState).Name));
+    }
+
+    void Update()
+    {
+        // Update parameters
+
+        // Enemy
+        var enemies = perception.GetGameObjects();
+        enemySeen.value = enemies.Length != 0;
+        enemy = (enemies.Length != 0) ? enemies[0] : null;
+        enemyHealth.value = (enemy != null) ? (enemy.GetComponent<Agent>().health) : 0f;
+        enemyDistance.value = (enemy != null) ? (Vector3.Distance(transform.position, enemy.transform.position)) : float.MaxValue;
+
+        timer.value -= Time.deltaTime;
+
+        stateMachine.Update();
+
+        animator.SetFloat("speed", movement.velocity.magnitude);
+    }
+
+    private void OnGUI()
+    {
+        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
+
+        GUI.Label(new Rect(screen.x, Screen.height - screen.y, 300, 20), stateMachine.GetStateName());
+        GUI.Label(new Rect(screen.x, Screen.height - screen.y - 10, 300, 20), $"health: {health.value}");
     }
 
     public static Agent[] GetAgents()
--- Assets/Scripts/AI/States/RetreatState.cs
+++ Assets/Scripts/AI/States/RetreatState.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EvadeState : State
+public class RetreatState : State
 {
     float angle;
     float distance;
 
-    public EvadeState(Agent owner, string name) : base(owner, name)
+    public RetreatState(Agent owner, string name) : base(owner, name)
     {
 
     }
@@ -21,16 +21,16 @@ public class EvadeState : State
         owner.perception.distance = 10;
     }
 
-    public override void OnExit()
-    {
-        owner.perception.angle = angle;
-        owner.perception.distance = distance;
-    }
-
     public override void OnUpdate()
     {
         Vector3 direction = (owner.transform.position - owner.enemy.transform.position).normalized; 
 
         owner.movement.MoveTowards(owner.transform.position + direction);
+    }
+
+    public override void OnExit()
+    {
+        owner.perception.angle = angle;
+        owner.perception.distance = distance;
     }
 }
